c3a8719620c68ffbadcf07644c5de4fc783cb8ab
Ankit Rawal <18305@iiitu.ac.in>
2021-03-04 16:05:19 +0530

    Adding more questions

---
+++ Array/Arrayleftreversbyd.cpp
@@ -0,0 +1,69 @@
+//Program to Cyclically rotate an array by k
+//
+//
+
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+void reversedarray(ll ar[],ll s,ll e){
+    while(s<=e){
+        ll x=ar[e];
+        ar[e]=ar[s];
+        ar[s]=x;
+        s++;
+        e--;
+    }
+}
+ 
+int main() {
+    ll n,k;
+    cin>>n>>k;
+    ll ar[n];
+  
+    for(ll i=0;i<n;i++){
+     cin>>ar[i];
+     
+    }
+    reversedarray(ar,0,k-1);
+    reversedarray(ar,k,n-1);
+    reversedarray(ar,0,n-1);
+    for(ll i=0;i<n;i++){
+       cout<<ar[i]<<" ";
+    }
+
+
+    return 0;
+}
+
+
+
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Array/KadaneAlgo.cpp
@@ -0,0 +1,64 @@
+//Program to Cyclically rotate an array by k
+//
+//Code fails when all numbers are nagative
+
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ 
+ 
+int main() {
+    ll n;
+    cin>>n;
+    ll ar[n];
+  
+    for(ll i=0;i<n;i++){
+     cin>>ar[i];
+    }
+    ll sum=0,num=0;
+    for(ll i=0;i<n;i++){
+      num+=ar[i];
+      if(num<0)
+       num=0;
+      sum=max(num,sum);
+      
+    }
+    cout<<sum<<"\n";
+     
+
+
+    return 0;
+}
+
+
+
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
